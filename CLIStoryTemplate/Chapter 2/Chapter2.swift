c0c12031a9548111d5c5038096bf5bde610df0c4
HKhafajii <alkhafajihassan@gmail.com>
2023-10-18 16:19:12 -0400

    Added print statement

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -7,4 +7,7 @@ import Foundation
 
 func chapterTwo() {
     // Your portion of the story goes here
+    
+    print("Brodie got shot dead")
+    print("BRodie is so damn confused he needs help")
 }
