7190d43f9194e5f2dd5f3773a7d05929ed1a8f98
HKhafajii <alkhafajihassan@gmail.com>
2023-10-19 10:18:34 -0400

    Added a chapter heading

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -8,5 +8,5 @@ import Foundation
 func chapterOne() {
     // Your portion of the story goes here
     
-   print("In 2057, a world gone completely in the direction expected by the most influential scientist 34 years ago.")
+   print("Chapter 1 The Beginning of The End \nIn 2057, a world gone completely in the direction expected by the most influential scientist 34 years ago.")
 }
