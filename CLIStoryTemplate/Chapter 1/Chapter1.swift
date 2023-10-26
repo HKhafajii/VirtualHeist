59b287a387407e92b9b8965b152b4dcb23f56058
HKhafajii <alkhafajihassan@gmail.com>
2023-10-26 15:36:33 -0400

    Added a whole lot of stuff!

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -15,49 +15,54 @@ func chapterOne() {
     
     
     
-    func survive() {
+    func survive(alive: Bool) -> String {
         if alive == true {
-            print("\(names) live to continue this great adventure!")
+           return "\(names) live to continue this great adventure!"
         }
         else {
-            print("Oh no! \(names) have perished, try again :(")
+            return "Oh no! \(names) have perished, try again :("
             
         }
     }
     func Introduction() {
-        print("Chapter 1 The Beginning of The End \n\tIn 2057, a world gone completely in the direction expected by the most influential scientist 34 years ago.")
-        print("A group of friends, \(names), grew up on the East side of Wisconson, where they attended Mitt Romney High. These three we're very big into the virtual world, to the point they started they're own club, \(club)")
-        print("Today they decided to enter \(game), and fight the newest challenge made by the great corruption that darkens their world.")
+        print("The Fiery Manuever \n\tIn 2057, a world gone completely in the direction expected by the most influential scientist 34 years ago. A group of friends, \(names), grew up on the East side of Wisconson, where they attended Mitt Romney High. These three we're very big into the virtual world, to the point they started they're own club, \(club). Today they decided to enter \(game), and fight the newest challenge made by the great corruption that darkens their world.")
     }
-    func chall1(){
+    func chall1(haveBoots: Bool, haveInvis: Bool, haveBlaster: Bool){
         let blaster = "Laser Blazer 9000"
         let boots = "Fly Guys 25's"
         let invisibility = "Dark Doagans Invisibility potion"
-        var haveBoots = false
-        var haveInvis = false
-        var haveBlaster = false
+        
         
         
         print("\(names) come up with the decision to pick their first tool of the day. They can etiher pick up the \(blaster), the \(boots), or \(invisibility). Choose 1 for \(blaster), 2 for \(boots), 3 for \(invisibility)")
+        let userInput = readLine()
+        
+//        if let int = Int(userInput) {
+//            
+//        }
+        let int = userInput
+       
         
         
         if haveBlaster == true {
             alive.toggle()
             print("You can't shoot through dragon skin!")
-            survive()
+            print(survive(alive: false))
             return
             }
         else if haveInvis == true {
                 alive.toggle()
                 print("The dragon just burned the whole room!")
-                survive()
+                print(survive(alive: false))
             return
         }
         else {
             print("Good choice, \(names) used the boots and got away!")
-            survive()
+            print(survive(alive: true))
+            
             
         }
+        
         
         
         
@@ -72,7 +77,8 @@ func chapterOne() {
         
         
     }
-    print(Introduction())
+    Introduction()
+    chall1(haveBoots: false, haveInvis: true, haveBlaster: false)
     
  
         
